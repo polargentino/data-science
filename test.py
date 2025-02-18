b8262d8abb6d3717e27615ff11d189da916a2091
Pablo Matías Monsalvo <polargentino@gmail.com>
2025-02-18 12:31:13 -0300

    corrección de test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pandas as pd
-import panda as np
+import numpy as np  # Cambia test_panda a numpy
 
 print("TensorFlow version:", tf.__version__)
 print("Pandas version:", pd.__version__)
-print("NumPy version:", np.__version__)
+print("NumPy version:", np.__version__)  # Ahora esto funcionará correctamente
